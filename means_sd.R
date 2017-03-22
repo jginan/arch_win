b752257908687279d6a5b83a5decabef8a62cd5d
esteful <e_kalpar@hotmail.com>
2017-03-22 01:03:44 +0100

    means_sd
    This is the script for the table with the means and SD of the
    concentrations

---
+++ means_sd.R
@@ -0,0 +1,22 @@
+means_sd <- function(x) {
+
+ 
+sum <-
+ numSummary(x, statistics=c("mean", "sd", "cv")
+ )
+
+sum$table -> tablaxxx
+
+tablaxxx[,3]*100 -> RSD
+
+cbind(tablaxxx[,-c(3)], RSD) -> tf100
+tf100
+
+round(tf100,2)-> tf100r
+cbind(tf100r, sum$n) ->tf100rn
+print(tf100rn)
+
+
+write.xlsx(tf100rn,/working_directory/means_sd.xlsx")
+
+} 
